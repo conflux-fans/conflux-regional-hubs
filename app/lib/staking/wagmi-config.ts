85a825fbd63558a5a2be1ddf061e12024f7d30ac
Pana <pana.wang@outlook.com>
2026-08-28 18:12:10 +0800

    fix: import Conflux chain from viem

--- app/lib/staking/wagmi-config.ts
+++ app/lib/staking/wagmi-config.ts
@@ -1,5 +1,5 @@
 import { createConfig, http } from "wagmi";
-import { confluxESpace } from "wagmi/chains";
+import { confluxESpace } from "viem/chains";
 
 export const wagmiConfig = createConfig({
   chains: [confluxESpace],
